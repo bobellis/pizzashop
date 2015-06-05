ad43ac395e5dc987f1ebe2c9f4bd57e6724d6d12
Bob Ellis <bobellis.tx@gmail.com>
2015-06-05 09:34:38 -0700

    Add order object that contains pizzas, add spec to test

--- js/scripts.js
+++ js/scripts.js
@@ -3,33 +3,11 @@ function Pizza(pizzaSize, toppings) {
   this.toppings = toppings;
 }
 
-Ticket.prototype.seniorDiscount = function() {
-  this.ticketPrice -= 6;
-};
+function Order(pizza){
+  this.pizzas = [pizza]
+}
 
-Ticket.prototype.matineeDiscount = function() {
-  this.ticketPrice -= 2;
-};
 
-$(document).ready(function(){
-
-$("form#movie1").submit(function(event) {
-  event.preventDefault();
-  var movieName = $('#movie1-title').text;
-  var movieTime = $('select#movie1-time option:selected').val();
-  var ticketPrice = 12;
-  debugger;
-
-  newTicket = new Ticket(movieName, movieTime, ticketPrice);
-  if ($("input#movie1-senior").checked){
-    newTicket.seniorDiscount();
-  }
-  else if (newTicket.movieTime ==="11:00am"){
-    newTicket.matineeDiscount();
-  }
-
-  $("#ticket-info").text("Your ticket for " + newTicket.movieName + " at " + newTicket.movieTime + "will cost $ " + newTicket.ticketPrice);
-
-});
-
-});
+Order.prototype.addPizza = function(pizza) {
+  this.pizzas = this.pizzas.push(pizza);
+}
--- spec/specs.js
+++ spec/specs.js
@@ -5,3 +5,11 @@ describe('pizza', function(){
     expect(newPizza.toppings).to.eql(["onions","pepporoni"]);
   });
 });
+
+describe('order', function(){
+  it("creates a new order object that contains pizza/s", function(){
+    var newPizza = new Pizza("large", ["onions", "pepporoni"]);
+    var newOrder = new Order(newPizza);
+    expect(newOrder.pizzas).to.eql([newPizza]);
+  });
+});
