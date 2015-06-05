053e761a3f3d6410b2c125ba58afe72077bdb891
Bob Ellis <bobellis.tx@gmail.com>
2015-06-05 09:50:53 -0700

    add prototype to calculate cost of an indivdual pizza

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,27 @@ function Pizza(pizzaSize, toppings) {
 }
 
 
+Pizza.prototype.pizzaCost = function(){
+  var pizzaCost = 0;
+  switch (this.pizzaSize){
+    case "large":
+      pizzaCost = 16;
+      break;
+    case "medium":
+      pizzaCost = 14;
+      break;
+    case "small":
+      pizzaCost = 12;
+      break;
+  }
+  pizzaCost += (this.toppings.length)
+  return pizzaCost;
+}
 
 function Order(pizza){
-  this.pizzas = [pizza]
+  this.pizzas = [pizza];
 }
+
 
 
 Order.prototype.addPizza = function(pizza) {
--- spec/specs.js
+++ spec/specs.js
@@ -1,22 +1,27 @@
 describe('pizza', function(){
   it("creates a new pizza object and takes size and toppings as arguments", function(){
-    var newPizza = new Pizza("large", ["onions","pepporoni"]);
+    var newPizza = new Pizza("large", ["onions","pepperoni"]);
     expect(newPizza.pizzaSize).to.equal("large");
-    expect(newPizza.toppings).to.eql(["onions","pepporoni"]);
+    expect(newPizza.toppings).to.eql(["onions","pepperoni"]);
+  });
+
+  it("allows us to calculate the cost of a pizza", function(){
+    var newPizza = new Pizza("large", ["onions", "pepperoni"]);
+    expect(newPizza.pizzaCost()).to.equal(18)
   });
 });
 
 describe('order', function(){
   it("creates a new order object that contains pizza/s", function(){
-    var newPizza = new Pizza("large", ["onions", "pepporoni"]);
+    var newPizza = new Pizza("large", ["onions", "pepperoni"]);
     var newOrder = new Order(newPizza);
     expect(newOrder.pizzas).to.eql([newPizza]);
   });
 
   it("allows a pizza to be added to an order", function(){
-    var newPizza = new Pizza("large", ["onions", "pepporoni"]);
+    var newPizza = new Pizza("large", ["onions", "pepperoni"]);
     var newOrder = new Order(newPizza);
-    var newPizza2 = new Pizza("medium", ["onions", "pepporoni"]);
+    var newPizza2 = new Pizza("medium", ["onions", "pepperoni"]);
     newOrder.addPizza(newPizza2);
     expect(newOrder.pizzas).to.eql([newPizza, newPizza2]);
   });
